c5d5953c922e8e2a65fd0d774539b32d2b409415
Benjamin Pope <buddypope1234@gmail.com>
2023-03-29 18:57:14 -0500

    fixing complining issues

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ function Contact() {
                 <h2><i class="fa-sharp fa-solid fa-circle-user"></i> Mercedies Pope</h2>
                 <h3><i class="fa-solid fa-mobile-screen-button"></i> 867-5309</h3>
                 <h3><i class="fa-solid fa-at"></i> shultsbens@yahoo.com</h3>
-                <h3><i class="fa-brands fa-facebook"></i>  <a href="https://www.facebook.com/groups/1404796009894589" target="_blank">Sassy Shirt Facebook Page</a></h3>
+                <h3><i class="fa-brands fa-facebook"></i>  <a href="https://www.facebook.com/groups/1404796009894589" rel="noreferrer" >Sassy Shirt Facebook Page</a></h3>
             </div>
         </div>
     )
